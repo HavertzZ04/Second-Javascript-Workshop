d4114e38be5444e992c3cefc71fa21f8b7b5a182
HavertzZ04 <havertzz04@gmail.com>
2023-03-12 22:07:36 -0500

    feat: :ambulance: Sixth Exercise
    I just remove the previous code with the objective to add the sixth exercise, this one works for any number of students reading the name, the sex and the final grade and find the student with the highest grade and the student with the lowest grade and how many were men and how many women.

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-/*Build the algorithm that reads two numbers from the keyboard,
-If the first is greater than the second, report their sum and
-difference, otherwise, inform the product and the
-division of the first by the second.*/
+/*Build the algorithm in Javascript for a program
+for any number of students reading the name,
+the sex and the final grade and find the student with the highest
+grade and the student with the lowest grade and how many were
+men and how many women.*/
 
-let n1 = parseInt(prompt("First number: "));
-let n2 = parseInt(prompt("Second number: "));
+let namesList = [];
+let sex = "";
+let gradelist = [];
+let m = [];
+let f = [];
 
-if(n1 > n2){
-    alert(`${n1} is greater than ${n2}, their sume is ${(n1 + n2)} and their difference is ${(n1 - n2)}`)
-}
-else {
-    alert(`${n1} is less than ${n2}, their product is ${n1 * n2} and their division is ${n2 / n1}`)
-}
+do {
+    namesList.push(prompt("Student name: "));
+
+    sex = (prompt("Student gender (M or F): "));
+
+    if(sex == "M" || sex == "m"){
+        m.push(1);
+    } else if(sex == "F" || sex == "f"){
+        f.push(1);
+    }
+
+    gradelist.push(parseFloat(prompt("Definitive grade (0.0 - 5.0): ")));
+
+} while(confirm("Wanna continue? "));
+
+let maxStudent = gradelist.indexOf(Math.max(...gradelist));
+let minStudent = gradelist.indexOf(Math.min(...gradelist));
+
+alert(`${namesList[maxStudent]} had the best grade: ${Math.max(...gradelist)} and ${namesList[minStudent]} had the worst grade was ${Math.min(...gradelist)}. There were ${m.length} boys and ${f.length} girls`);
