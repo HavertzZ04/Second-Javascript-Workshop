3548858202722013fdece5f10a8f32e8996e037d
HavertzZ04 <havertzz04@gmail.com>
2023-03-12 21:59:28 -0500

    feat: :ambulance: Fourth Exercise
    I just remove the previous code with the objective to add the fourth exercise, this one requests three names and their ages in order to check who is the oldest person

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-/*
-Build the algorithm to determine the voltage of a
-circuit based on resistance and current.*/
+/*Build the algorithm that requests the name and age of 3
+people and determine the name of the oldest person.*/
 
-let i = parseInt(prompt("Write the current intensity: "));
-let r = parseInt(prompt("Write the resistance: "));
-let v = (i * r);
+let person1 = prompt("Name of the first person: ");
+let age1 = prompt("Age of the first person: ");
+let person2 = prompt("Name of the second person: ");
+let age2 = prompt("Age of the second person: ");
+let person3 = prompt("Name of the third person: ");
+let age3 = prompt("Age of the third person: ");
 
-alert(`The voltage is ${v}`);
+let total = Math.max(age1, age2, age3);
+
+if(total == age1){
+    alert(`${person1} is ${age1} years old and for that reason is the oldest`)
+}
+else if (total == age2){
+    alert(`${person2} is ${age2} years old and for that reason is the oldest`)
+}
+else {
+    alert(`${person3} is ${age3} years old and for that reason is the oldest`)
+}
